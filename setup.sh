77afd4da93093bc1d0a69f5422fbf57210703b3f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-07 22:22:13 +0900

    Modify setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
+# Disable Screenshot's shadow
 # ref. https://dackdive.hateblo.jp/entry/2014/01/20/122818
 defaults write com.apple.screencapture disable-shadow -boolean true
-killall SystemUIServer
 
 # Change Screenshot file name and location
-cd ~/Desktop
-mkdir Screenshots
+mkdir -p ~/Desktop/Screenshots
 defaults write com.apple.screencapture location ~/Desktop/Screenshots
 defaults write com.apple.screencapture name SS
-killall SystemUIServer
-  
+
 # Show hidden files/folders on Finder
 defaults write com.apple.finder AppleShowAllFiles -boolean true
-killall Finder 
+
+killall Finder
+killall SystemUIServer
